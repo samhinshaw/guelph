8e19b39e6479b6f3ee49292b68d48e42b0c5fcf7
Sam Hinshaw <samuel.hinshaw@gmail.com>
2015-11-21 20:22:45 -0800

    updated man for factorchars

--- R/factorchars.R
+++ R/factorchars.R
@@ -1,7 +1,7 @@
 #' Take a column in a data.frame and coerce it from factors to characters.
 #'
 #' @param q Your data.frame
-#' @param ... Your factor column(s)
+#' @param r The column you wish to convert from factors to characters (as a character, like "r")
 #'
 #' @return data.frame
 #' @export
@@ -9,9 +9,9 @@
 #' @examples
 #' factorchars(iris, "Species")
 factorchars <- function(q, r) {
-    Rlist <- q[[paste(r)]]
+    Rlist <- q[[r]]
     Rlist <- as.character(Rlist)
-    q[[paste(r)]] <- Rlist
+    q[[r]] <- Rlist
     return(q)
 }
 
